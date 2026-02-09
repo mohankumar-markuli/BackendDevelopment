00b1d2ad012f139d940f106581a535d4363756a5
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-02-09 17:08:36 +0530

    practice

--- JavaScript/20_practice.js
+++ JavaScript/20_practice.js
@@ -5,14 +5,19 @@ const student = [
   { firstName: "Mohan", lastName: "Kumar", age: 29 },
 ];
 
+// map function implementation
 const map_output = student.map(function displayFullName(arr) {
   return arr.firstName + " " + arr.lastName;
 });
 console.log(map_output);
 
-function fullName(obj_collection) {
-  return obj_collection.firstName + " " + obj_collection.lastName;
-}
+// filter function implementation
+const filter_output = student.filter(function age(arr) {
+  return arr.age > 30;
+});
+console.log("filter", filter_output);
+
+// reduce function implementation
 
 function fetch_data(collection, operation) {
   output_collection = [];
@@ -23,4 +28,21 @@ function fetch_data(collection, operation) {
   return output_collection;
 }
 
+function fullName(objCollection) {
+  return objCollection.firstName + " " + objCollection.lastName;
+}
+
 console.log(fetch_data(student, fullName));
+
+// reduce to count values in object collection
+
+const ageCount = student.reduce(function ageCount(acc, curr) {
+  if (acc[curr.age]) {
+    acc[curr.age] = ++acc[curr.age];
+  } else {
+    acc[curr.age] = 1;
+  }
+  return acc;
+}, {});
+
+console.log(ageCount);
