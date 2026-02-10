5293c1548159a9dde0a6ee65be316d62d1c3bd67
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-02-10 18:46:04 +0530

    example

--- JavaScript/26_example_await.js
+++ JavaScript/26_example_await.js
@@ -2,7 +2,7 @@ const API_URL = "https://api.github.com/users/mohankumar-markuli";
 
 async function handlePromise() {
   try {
-    const data = await fetch(API_URL_);
+    const data = await fetch(API_URL);
     const jsonValue = await data.json();
     console.log(jsonValue);
   } catch (err) {
